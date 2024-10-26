69d24363382338e8e4a6beca0a852e17aeb4a61b
Artiushchenko <Artyushchenko2018@gmail.com>
2024-10-26 15:28:51 +0300

    Operations list was realized

---
+++ src/components/screens/home/transactions/transaction-item/transaction-item.component.js
@@ -0,0 +1,41 @@
+import ChildComponent from '@/core/component/child.component'
+import { $K } from '@/core/kquery/kquery.lib'
+import renderService from '@/core/services/render.service'
+
+import { formatToCurrency } from '@/utils/format/format-to-currency.util'
+import { formatDate } from '@/utils/format/format-to-date.util'
+
+import * as styles from './transaction-item.module.scss'
+import template from './transaction-item.template.html'
+
+export class TransactionItem extends ChildComponent {
+	constructor(transaction) {
+		super()
+
+		this.transaction = transaction
+	}
+
+	render() {
+		this.element = renderService.htmlToElement(template, [], styles)
+
+		const isIncome = this.transaction.type === 'TOP_UP'
+
+		const name = isIncome ? 'Income' : 'Expense'
+
+		if (isIncome) {
+			$K(this.element).addClass(styles.income)
+		}
+
+		$K(this.element).find('#transaction-name').text(name)
+
+		$K(this.element)
+			.find('#transaction-date')
+			.text(formatDate(this.transaction.createdAt))
+
+		$K(this.element)
+			.find('#transaction-amount')
+			.text(formatToCurrency(this.transaction.amount))
+
+		return this.element
+	}
+}
