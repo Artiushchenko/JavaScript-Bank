c60182189296a75eb64aaccc2958cf09c08ef49c
Artiushchenko <Artyushchenko2018@gmail.com>
2024-10-26 21:13:02 +0300

    Transaction statistics SVG graph was implemented

--- src/components/screens/home/statistics/statistics.component.js
+++ src/components/screens/home/statistics/statistics.component.js
@@ -16,6 +16,7 @@ import { StatisticService } from '@/api/statistic.service'
 import * as styles from './statistics.module.scss'
 import template from './statistics.template.html'
 
+import { CircleChart } from './circle-chart/circle-chart.component'
 import { StatisticsItem } from './statistics-item/statistics-item.component'
 import { TRANSACTION_COMPLETED } from '@/constants/event.constants'
 
@@ -57,6 +58,25 @@ export class Statistics extends ChildComponent {
 		this.#removeListeners()
 	}
 
+	renderChart(income, expense) {
+		const total = income + expense
+
+		let incomePercent = (income * 100) / total,
+			expensePercent = 100 - incomePercent
+
+		if (income && !expense) {
+			incomePercent = 100
+			expensePercent = 0.1
+		}
+
+		if (!income && expense) {
+			incomePercent = 0.1
+			expensePercent = 100
+		}
+
+		return new CircleChart(incomePercent, expensePercent).render()
+	}
+
 	fetchData() {
 		this.statisticService.main(data => {
 			if (!data) {
@@ -74,8 +94,8 @@ export class Statistics extends ChildComponent {
 			)
 			statisticsItemsElement.text('')
 
-			// const circleChartElement = $K(this.element).find('#circle-chart')
-			// circleChartElement.text('')
+			const circleChartElement = $K(this.element).find('#circle-chart')
+			circleChartElement.text('')
 
 			statisticsItemsElement
 				.append(
@@ -92,6 +112,10 @@ export class Statistics extends ChildComponent {
 						'purple'
 					).render()
 				)
+
+			circleChartElement.append(
+				this.renderChart(data[0].value, data[1].value)
+			)
 		})
 	}
 
